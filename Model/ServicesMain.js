493376d2bd58b870f68ced75babfde90a273a545
Akhiltop <akhilyadlapalli@gmail.com>
2023-07-20 17:01:37 +0530

    added number

--- Model/ServicesMain.js
+++ Model/ServicesMain.js
@@ -1,14 +1,15 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const servicesMainSchema=mongoose.Schema({
-    smname:{type:String,required:true},
-    image:{type:String,required:true},
-    description:{type:String,require:true},
-    sdescription:{type:String,require:true}
-})
+const servicesMainSchema = mongoose.Schema({
+  smname: { type: String, required: true },
+  image: { type: String, required: true },
+  description: { type: String, require: true },
+  sdescription: { type: String, require: true },
+  starting: { type: Number },
+});
 
-const ServicesMainModel=mongoose.model("servicesMain",servicesMainSchema)
+const ServicesMainModel = mongoose.model("servicesMain", servicesMainSchema);
 
-module.exports={
-    ServicesMainModel
-}
+module.exports = {
+  ServicesMainModel,
+};
