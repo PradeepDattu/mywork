8d695c1eb548b9a9d662cf7ad69eaab0b2cadf4a
Akhiltop <akhilyadlapalli@gmail.com>
2023-07-30 22:35:51 +0530

    modified revenue

--- Route/Revenue.js
+++ Route/Revenue.js
@@ -19,11 +19,9 @@ const { HoroModel } = require("../Model/Horoscope");
 //       console.log(start,end,eventDate)
 //       return eventDate >= start && eventDate <= end;
 //     });
-    
-//     console.log(eventsInRange);
-    
 
-    
+//     console.log(eventsInRange);
+
 //     res.send("rev");
 //   } catch (error) {
 //     console.log(error);
@@ -33,7 +31,7 @@ const { HoroModel } = require("../Model/Horoscope");
 function convertToDate(dateStr) {
   const dateComponents = dateStr.split("/");
   // Date constructor expects date components in the order: year, month, day
-  return new Date(dateComponents[2], dateComponents[1], dateComponents[0]);
+  return new Date(dateComponents[2], dateComponents[1] - 1, dateComponents[0]);
 }
 RevenueRoute.get("/", async (req, res) => {
   const startDateStr = req.query.from;
@@ -42,30 +40,35 @@ RevenueRoute.get("/", async (req, res) => {
   // Date constructor expects date components in the order: year, month, day
   const startDate = convertToDate(startDateStr);
   const endDate = convertToDate(endDateStr);
+  // console.log(startDate, endDate);
   try {
     const events = await EventModel.find();
     const eventsInRange = events.filter((event) => {
       const eventDate = convertToDate(event.eventDate);
       return eventDate >= startDate && eventDate <= endDate;
     });
+    // console.log(eventsInRange);
     const astroData = await HoroModel.find();
-    const astroInRange=astroData.filter(asto=>{
-      const horoDate=convertToDate(asto.horoDate)
-      return horoDate>= startDate && horoDate<=endDate
-    })
+    const astroInRange = astroData.filter((asto) => {
+      const horoDate = convertToDate(asto.horoDate);
+      return horoDate >= startDate && horoDate <= endDate;
+    });
     const rev = new RevenueModel();
 
     rev.paid =
-    eventsInRange
+      eventsInRange
         .filter((dat) => dat.paymentStatus == true)
         .reduce((total, event) => total + event.ammount, 0) +
-        astroInRange
+      astroInRange
         .filter((dat) => dat.paymentStatus == true)
         .reduce((total, event) => total + event.ammount, 0);
     rev.unpaid = eventsInRange
       .filter((dat) => dat.paymentStatus == false)
       .reduce((total, event) => total + event.ammount, 0);
-    rev.expense = eventsInRange.reduce((total, event) => total + event.expense, 0);
+    rev.expense = eventsInRange.reduce(
+      (total, event) => total + event.expense,
+      0
+    );
     rev.transaction = rev.paid + rev.unpaid;
     rev.net = rev.transaction - rev.expense;
 
