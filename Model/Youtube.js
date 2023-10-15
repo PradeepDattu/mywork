86e1ec828cd7dc2f6383493ebba44a5d4353c044
Akhiltop <akhilyadlapalli@gmail.com>
2023-10-16 00:51:17 +0530

    updated youtube

--- Model/Youtube.js
+++ Model/Youtube.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const youTubeLinkSchema = new mongoose.Schema({
+const youtubeSchema = new mongoose.Schema({
   link1: String,
   link2: String,
   link3: String,
   link4: String,
 });
 
-module.exports = mongoose.model("YouTubeLink", youTubeLinkSchema);
+const YoutubeModel = mongoose.model("youtube", youtubeSchema);
+
+module.exports = {
+  YoutubeModel,
+};
