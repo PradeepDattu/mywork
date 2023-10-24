98e4d616870ba32b20f6215c9eb4e87ed8229cf6
Akhiltop <akhilyadlapalli@gmail.com>
2023-10-24 17:23:09 +0530

    added gothram refered

--- Model/EventBooking.js
+++ Model/EventBooking.js
@@ -2,26 +2,33 @@ const mongoose = require("mongoose");
 
 const eventSchema = mongoose.Schema({
   fname: { type: String, required: true },
-  lname: { type: String,  },
-  userId:{type:String,required: true},
-  eventId:{type:String,default:null},
-  eventName: { type: String,default:null },
+  lname: { type: String },
+  userId: { type: String, required: true },
+  eventId: { type: String, default: null },
+  eventName: { type: String, default: null },
   phone: { type: String, required: true },
-  bookingDate:{ type: String, required: true },
-  email: { type: String},
-  pincode: { type: String, require: true }, 
+  bookingDate: { type: String, required: true },
+  email: { type: String },
+  pincode: { type: String, require: true },
   eventDate: { type: String, require: true },
-  city:{ type: String, default:null },
+  city: { type: String, default: null },
   message: { type: String },
-  eventStatus:{type:String,default:"Pending"},
-  eventTime:{type:String,default:"00:00"},
-  image:{type:String,default:null},
-  paymentStatus:{type:Boolean,default:false},
-  ammount:{type:Number,default:0},
-  expense:{type:Number,default:0},
+  referedby: { type: String },
+  eventStatus: { type: String, default: "Pending" },
+  eventTime: { type: String, default: "00:00" },
+  image: { type: String, default: null },
+  paymentStatus: { type: Boolean, default: false },
+  ammount: { type: Number, default: 0 },
+  expense: { type: Number, default: 0 },
   address: { type: String, require: true },
   district: { type: String },
-  authorMessage: { type: String,default: null }
+  authorMessage: { type: String, default: null },
+  gothramDetails: [
+    {
+      pname: String,
+      pgothram: String,
+    },
+  ],
 });
 
 const EventModel = mongoose.model("EventBooking", eventSchema);
--- Model/Horoscope.js
+++ Model/Horoscope.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 
 const horoSchema = mongoose.Schema({
-  fname:{ type: String, required: true },
-  lname:{ type: String },
-  userId:{type:String,required:true},
-  email:{type:String},
+  fname: { type: String, required: true },
+  lname: { type: String },
+  userId: { type: String, required: true },
+  email: { type: String },
   phone: { type: String, required: true },
-  address:{type:String,required: true},
-  DOB:{type:String,default:null},
-  city:{ type: String, default:null },
-  TOB:{type:String,default:null},
-  POB:{type:String,default:null},
-  message:{type: String},
-  nakshatra:{type: String},
-  image:{type:String,default:null},
-  paymentStatus:{type:Boolean,default:false},
-  ammount:{type:Number,default:0},
-  horoStatus:{type:String,default:"Pending"},
-  horoDate:{type:String},
-  authorMessage: { type: String,default: null }
+  address: { type: String, required: true },
+  DOB: { type: String, default: null },
+  city: { type: String, default: null },
+  TOB: { type: String, default: null },
+  POB: { type: String, default: null },
+  message: { type: String },
+  referedby: { type: String },
+  nakshatra: { type: String },
+  image: { type: String, default: null },
+  paymentStatus: { type: Boolean, default: false },
+  ammount: { type: Number, default: 0 },
+  horoStatus: { type: String, default: "Pending" },
+  horoDate: { type: String },
+  authorMessage: { type: String, default: null },
 });
 
 const HoroModel = mongoose.model("horoScope", horoSchema);
@@ -26,4 +27,3 @@ const HoroModel = mongoose.model("horoScope", horoSchema);
 module.exports = {
   HoroModel,
 };
-  
